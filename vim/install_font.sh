faad31e2c34e425b725bd3299f1b334619c6a383
naoya-sugiura <nsugi1010@gmail.com>
2021-04-06 18:21:32 +0900

    update font name

--- vim/install_font.sh
+++ vim/install_font.sh
@@ -7,7 +7,7 @@ font_dir="$HOME/.local/share/fonts"
 
 mkdir -p $font_dir
 cd $font_dir
-curl -fLo "Caskaydia Cove Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/CascadiaCode/complete/Caskaydia%20Cove%20Regular%20Nerd%20Font%20Complete.ttf
+curl -fLo "Caskaydia Cove Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/CascadiaCode/Regular/complete/Caskaydia%20Cove%20Regular%20Nerd%20Font%20Complete.otf
 curl -fLo "Ubuntu Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Ubuntu/Regular/complete/Ubuntu%20Nerd%20Font%20Complete.ttf
 
 fc-cache -f "$font_dir"
